59b4059ca57883ed854e27f70b16c40a3d1892ea
EC2 Default User <ec2-user@ip-172-31-41-231.ec2.internal>
2021-04-04 04:51:25 +0000

    javascript課題4提出

---
+++ 6/slick.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slick-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        dots: true,
+        arrows: true,
+        infinite: true,
+    });
+});
